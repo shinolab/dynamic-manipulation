53f861e0a9f1c93761774c8bff0301d8657ee47b
furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2018-06-27 19:43:42 +0900

    erased unnecessary comments

--- sample/simple.cpp
+++ sample/simple.cpp
@@ -21,33 +21,8 @@ int main()
 	odcs.Initialize();
 	std::vector<FloatingObjectPtr> objPtrs;
 	objPtrs.push_back(FloatingObjectPtr(new FloatingObject(Eigen::Vector3f(0, 0, 1350))));
-	//objs.push_back(FloatingObject(Eigen::Vector3f(-250, 100, 1485)));
+	//objs.push_back(FloatingObject(Eigen::Vector3f(-250, 100, 1485))); // add object
 	odcs.StartControl(objPtrs);
-	/*
-	while (1)
-	{
-	odcs.ods.DeterminePositionByDepthWithROI(objPtrs[0]);
-	//odcs.ods.DeterminePositionByDepth(objPtrs);
-	ofs << timeGetTime() - initialTime << ", "
-	<< objPtrs[0]->getPosition().x() << ","
-	<< objPtrs[0]->getPosition().y() << ","
-	<< objPtrs[0]->getPosition().z() << ","
-	<< objPtrs[0]->isTracked << std::endl;
-
-	//std::cout << objPtrs[0]->isTracked << std::endl;
-	auto key = cv::waitKey(1);
-	if (key == 'q')
-	{
-	cv::destroyAllWindows();
-	break;
-	}
-	}
-	*/
-	
-	//write your application process here.
-	//objPtrs[0]->updateStatesTarget(Eigen::Vector3f(200, 0, 1350), Eigen::Vector3f(0, 0, 0)); //Move Object
-	//Sleep(10000);
-	//objPtrs[0]->updateStatesTarget(Eigen::Vector3f(0, 0, 1350), Eigen::Vector3f(0, 0, 0)); //Move Object
 
 	std::cout << "Press any key to close." << std::endl;
 	getchar();

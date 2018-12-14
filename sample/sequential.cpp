c736793a27328e3f307a0a3b1c7766d8c227bf46
furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2018-12-14 15:02:50 +0900

    use focal point in sequential.cpp

--- sample/sequential.cpp
+++ sample/sequential.cpp
@@ -12,16 +12,16 @@
 
 int main()
 {
-	std::ofstream ofs("20181211_controlByFocus.csv");
+	std::ofstream ofs("20181214_controlByFocus2.csv");
 	ofs << "time[ms], succeeded, x(raw)[mm], y(raw)[mm], z(raw)[mm], "
 		<< "x(est)[mm], y(est)[mm], z(est)[mm], vx[mm/s], vy[mm/s], vz[mm/s], "
 		<< "Ix[mms], Iy[mms], Iz[mms], x_tgt[mm], y_tgt[mm], z_tgt[mm], "
 		<< "Fx[mN], Fy[mN], Fz[mN], u0, u1, u2, u3, u4" << std::endl;
-	const int period = 10000;
+	const int period = 20000;
 	const int loopPeriod = 30;
 	odcs odcs;
 	odcs.Initialize();
-	auto objPtr = FloatingObject::Create(Eigen::Vector3f(0, 0, 1350));
+	auto objPtr = FloatingObject::Create(Eigen::Vector3f(0, 0, 1350), 0.135e-3);
 	odcs.RegisterObject(objPtr);
 	int initTime = timeGetTime();
 	while (1)
@@ -41,7 +41,7 @@ int main()
 		case 3:
 			positionTarget << 200, 0, 1500; break;
 		case 4:
-			positionTarget << 0, 0, 1500; break;
+			positionTarget << 500, 0, 1500; break;
 		default:
 			exitFlag = true;
 			break;

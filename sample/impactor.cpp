098a3bd9ce3ead1d0a01221f4b82f8eb72cace3f
furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2019-01-23 12:17:23 +0900

    added profile.hpp and .cpp

--- sample/impactor.cpp
+++ sample/impactor.cpp
@@ -3,8 +3,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <fstream>
-
-//input
+#include <chrono>
 
 Eigen::Vector3f ComputeAnisotropicPIDForce(FloatingObjectPtr objPtr
 	, Eigen::Matrix3f directions

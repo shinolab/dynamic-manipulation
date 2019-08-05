12fed14ed00f3af23e83dfa7ecbe1601ee9e30c6
takuro <furumoto@hapis.k.u-tokyo.ac.jp>
2019-08-05 15:45:12 +0900

    added dynaman server

---
+++ server/dataStruct/query.hpp
@@ -0,0 +1,13 @@
+#ifndef _QUERY_SPHERE_TRACKER
+#define _QUERY_SPHERE_TRACKER
+
+enum orderID {
+	TEST = 0,
+	POSITION = 1
+};
+
+struct query {
+	orderID id;
+};
+
+#endif // !_QUERY_SPHERE_TRACKER
---
+++ server/server/server.hpp
@@ -0,0 +1,27 @@
+#ifndef _TCPIP_SERVER_HPP
+#define _TCPIP_SERVER_HPP
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <functional>
+
+#define BUFFER_SIZE 1024
+
+class server {
+public:
+	server(const std::function<void (const char*, char*)> &process, const int port);
+	~server();
+	void start();
+private:
+	sockaddr_in srcAddr;
+	sockaddr_in dstAddr;
+	WSAData data;
+	int srcSocket;
+	int dstSocket;
+	int dstAddrSize;
+	std::function<void (const char*, char*)> _process;
+	char queryBuffer[BUFFER_SIZE];
+	char replyBuffer[BUFFER_SIZE];
+	int status;
+
+};
+#endif

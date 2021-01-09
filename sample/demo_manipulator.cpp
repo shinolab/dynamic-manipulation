53a5fce7f1db8c4b51cc47505d7d74e70b3cb1df
furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2021-01-09 15:00:54 +0900

    added config files for demo_manipulator

--- sample/demo_manipulator.cpp
+++ sample/demo_manipulator.cpp
@@ -12,8 +12,7 @@ int main(int argc, char** argv ) {
 
 	/*user-defined configurations*/
 	Eigen::Vector3f pos_init(0, -50, 0);
-	//std::string target_image_name("blue_target_no_cover.png");
-	std::string target_image_name("blue_20cm_daiso_target.png");
+	std::string target_image_name("blue_target_no_cover.png");
 	/*end of user-defined configurations*/
 
 	auto pObject = dynaman::FloatingObject::Create(

483f476a3713995a2ddb1279cf177a5ffee90844
Takuro Furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2022-02-01 16:38:08 +0900

    erased VarMultiplexManipulator(deprecated)

--- inc/manipulator.hpp
+++ inc/manipulator.hpp
@@ -15,13 +15,6 @@ namespace dynaman {
 		virtual bool Navigate(std::shared_ptr<Tracker> pTracker, FloatingObjectPtr pObject) = 0;
 	};
 
-	class SimpleNavigator : Navigator {
-		std::ofstream ofs;
-	public:
-		void InitLog(const std::string& logName);
-		void CloseLog();
-		bool Navigate(std::shared_ptr<Tracker> pTracker, FloatingObjectPtr pObject) override;
-	};
 
 	class Manipulator {
 	public:
@@ -56,7 +49,6 @@ namespace dynaman {
 		std::string m_controlLogName;
 		bool m_logEnabled;
 		std::thread m_thr_control;
-		std::thread m_thr_track;
 		std::shared_mutex m_mtx_isRunning;
 		std::mutex m_mtx_isPaused;
 		std::mutex m_mtx_gain;
@@ -69,8 +61,7 @@ namespace dynaman {
 			const Eigen::Vector3f& gainD,
 			const Eigen::Vector3f& gainI,
 			float freqLm,
-			int loopPeriodAupa,
-			int loopPeriodTracker,
+			int loopPeriod,
 			float lambda,
 			std::shared_ptr<arfModelLinearBase> arfModelPtr
 		);
@@ -80,9 +71,8 @@ namespace dynaman {
 			const Eigen::Vector3f& gainD,
 			const Eigen::Vector3f& gainI,
 			float freqLm = 100.f,
-			int loopPeriodAupa = 10,
-			int loopPeriodTracker = 5,
-			float lambda = 0.f,
+			int loopPeriod = 11,
+			float lambda = 0.0f,
 			std::shared_ptr<arfModelLinearBase> arfModelPtr = std::make_shared<arfModelFocusSphereExp50mm>()//std::make_shared<arfModelFocusOnSphereExperimental>()
 		);
 
@@ -124,110 +114,8 @@ namespace dynaman {
 			const std::string& obsLogName,
 			const std::string& controlLogName
 		);
-		void DisableLog();
-	};
-
-	class VarMultiplexManipulator: public Manipulator {
-	private:
-		SimpleNavigator navigator;
-		Eigen::Vector3f m_gainP;
-		Eigen::Vector3f m_gainD;
-		Eigen::Vector3f m_gainI;
-		int m_periodMux_us;
-		int m_periodControl_ms;
-		int m_periodObs_ms;
-		int m_interval_min;
-		float m_lambda;
-		float m_duty_max;
-		std::shared_ptr<arfModelLinearBase> m_arfModelPtr;
-		bool m_isRunning;
-		bool m_isPaused;
-		bool m_logEnabled;
-		std::thread m_thr_control;
-		std::thread m_thr_track;
-		std::shared_mutex m_mtx_isRunning;
-		std::mutex m_mtx_isPaused;
-		std::mutex m_mtx_gain;
-		std::string m_controlLogName;
-		std::ofstream m_controlLogStream;
-	public:
-		std::shared_ptr<autd::Controller> m_pAupa;
-		std::shared_ptr<Tracker> m_pTracker;
-		FloatingObjectPtr m_pObject;
-		WinMultiplexer mux;
-
-		VarMultiplexManipulator(
-			const Eigen::Vector3f& gainP,
-			const Eigen::Vector3f& gainD,
-			const Eigen::Vector3f& gainI,
-			int periodMux_us,
-			int periodUpdateControl_ms,
-			int periodUpdateObs_ms,
-			int interval_min_us,
-			float duty_max,
-			float lambda,
-			std::shared_ptr<arfModelLinearBase> arfModelPtr
-		);
-
-		static std::shared_ptr<Manipulator> Create(
-			const Eigen::Vector3f& m_gainP,
-			const Eigen::Vector3f& m_gainD,
-			const Eigen::Vector3f& m_gainI,
-			int periodMux_us = 10000,
-			int updatePeriodAupa_ms = 10,
-			int updatePeriodTraceker_ms = 5,
-			int interval_min_us = 1000,
-			float duty_max = 1.0,
-			float lambda = 0.0f,
-			std::shared_ptr<arfModelLinearBase> arfModelPtr = std::make_shared<arfModelFocusSphereExp50mm>()
-		);
-
-		int StartManipulation(
-			std::shared_ptr<autd::Controller> pAupa,
-			std::shared_ptr<Tracker> pTracker,
-			FloatingObjectPtr pObject
-		) override;
-
-
-		void FinishManipulation() override;
-
-		void PauseManipulation() override;
-
-		void ResumeManipulation() override;
-
-		bool IsRunning();
-
-		bool IsPaused();
-
-		void ExecuteSingleActuation(
-			std::shared_ptr<autd::Controller> pAupa,
-			FloatingObjectPtr pObject
-		);
-
-		void ExecuteOnPaused(
-			std::shared_ptr<autd::Controller> pAupa,
-			FloatingObjectPtr pObject
-		);
-
-		Eigen::VectorXf ComputeDuty(const Eigen::Vector3f& forceTarget, const Eigen::Vector3f& focus);
-
-		std::vector<std::pair<autd::GainPtr, int>> CreateDriveSequence(const Eigen::VectorXf& duties, const Eigen::Vector3f& focus);
-
-		std::vector<std::pair<autd::GainPtr, int>> CreateDriveSequenceOld(const Eigen::VectorXf& duties, const Eigen::Vector3f& focus);
-
-		std::vector<std::pair<autd::GainPtr, int>> SplitSequence(const std::vector<std::pair<autd::GainPtr, int>>& sequence, int interval);
-
-		void SetGain(const Eigen::Vector3f& gainP, const Eigen::Vector3f& gainD, const Eigen::Vector3f& gainI);
-
-		std::shared_ptr<arfModelLinearBase> arfModel();
-
-		void EnableLog(
-			const std::string& obsLogName,
-			const std::string& controlLogName
-		);
 
 		void DisableLog();
 	};
-
 }
 #endif // !_DYNAMAN_STRATEGY_HPP
--- lib/manipulator.cpp
+++ lib/manipulator.cpp
@@ -30,8 +30,7 @@ namespace dynaman {
 		const Eigen::Vector3f& gainD,
 		const Eigen::Vector3f& gainI,
 		float freqLm,
-		int loopPeriodAupa,
-		int loopPeriodTracker,
+		int loopPeriod,
 		float lambda,
 		std::shared_ptr<arfModelLinearBase> arfModelPtr)
 		:m_gainP(gainP),
@@ -39,9 +38,7 @@ namespace dynaman {
 		m_gainI(gainI),
 		m_freqLm(freqLm),
 		m_lambda(lambda),
-		m_loopPeriodAupa(loopPeriodAupa),
-		m_loopPeriodTracker(loopPeriodTracker),
-		loopPeriod_(10),
+		loopPeriod_(loopPeriod),
 		m_arfModelPtr(arfModelPtr),
 		m_isRunning(false),
 		m_isPaused(false),
@@ -53,8 +50,7 @@ namespace dynaman {
 		const Eigen::Vector3f& gainD,
 		const Eigen::Vector3f& gainI,
 		float freqLm,
-		int loopPeriodAupa,
-		int loopPeriodTracker,
+		int loopPeriod,
 		float lambda,
 		std::shared_ptr<arfModelLinearBase> arfModelPtr
 	) {
@@ -63,8 +59,7 @@ namespace dynaman {
 			gainD,
 			gainI,
 			freqLm,
-			loopPeriodAupa,
-			loopPeriodTracker,
+			loopPeriod,
 			lambda,
 			arfModelPtr
 			);
@@ -136,7 +131,6 @@ namespace dynaman {
 			for (int i_autd = 0; i_autd < m_pAupa->geometry()->numDevices(); i_autd++) {
 				if (i_autd == std::distance(dutiesStl.begin(), itr_duties)) {
 					int amplitude = std::max(0, (std::min(255, static_cast<int>((*itr_duties) * 255.f * num_active))));
-					//std::cout << amplitude << std::endl;
 					gain_map.insert(std::make_pair(i_autd, autd::FocalPointGain::Create(focus, amplitude)));
 				}
 				else {
@@ -167,8 +161,6 @@ namespace dynaman {
 		if (m_logEnabled) {
 			m_obsLogStream << observeTime << "," << posObserved.x() << "," << posObserved.y() << "," << posObserved.z() << std::endl;
 		}
-		//int waitTime = m_loopPeriodTracker - (timeGetTime() - observeTime);
-		//Sleep(std::max(waitTime, 0));
 	}
 
 	void MultiplexManipulator::ExecuteSingleActuation(
@@ -231,8 +223,6 @@ namespace dynaman {
 				m_controlLogStream << std::endl;
 			}
 		}
-		//int waitTime = m_loopPeriodAupa - (timeGetTime() - timeLoopInit);
-		//Sleep(std::max(waitTime, 0));
 	}
 
 	int MultiplexManipulator::StartManipulation(
@@ -292,9 +282,6 @@ namespace dynaman {
 		}
 		if (m_thr_control.joinable()) {
 			m_thr_control.join();
-		}
-		if (m_thr_track.joinable()){
-			m_thr_track.join();
 		}
 		if (m_obsLogStream.is_open()) {
 			m_obsLogStream.close();
@@ -356,410 +343,5 @@ namespace dynaman {
 		if (!IsRunning()) {
 			m_logEnabled = false;
 		}
-	}
-
-	void SimpleNavigator::InitLog(const std::string& logName) {
-		ofs.open(logName);
-		ofs << "sys_time,x,y,z" << std::endl;
-	}
-
-	void SimpleNavigator::CloseLog() {
-		ofs.close();
-	}
-
-	bool SimpleNavigator::Navigate(std::shared_ptr<Tracker> pTracker, FloatingObjectPtr pObject) {
-		DWORD observationTime = timeGetTime();
-		Eigen::Vector3f posObserved;
-		bool observed = pTracker->observe(observationTime, posObserved, pObject);
-		if (observed && isInsideWorkspace(pObject->getPosition(), pObject->lowerbound(), pObject->upperbound())) {
-			pObject->updateStates(observationTime, posObserved);
-			pObject->SetTrackingStatus(true);
-		}
-		else if (observationTime - pObject->lastDeterminationTime > thres_timeout_track_ms) {
-			pObject->SetTrackingStatus(false);
-		}
-		if (ofs.is_open()) {
-			ofs << observationTime << "," << posObserved.x() << "," << posObserved.y() << "," << posObserved.z() << std::endl;
-		}
-		return observed;
-	}
-
-	VarMultiplexManipulator::VarMultiplexManipulator(
-		const Eigen::Vector3f& gainP,
-		const Eigen::Vector3f& gainD,
-		const Eigen::Vector3f& gainI,
-		int periodMux_us,
-		int periodControl_ms,
-		int periodObs_ms,
-		int interval_min_us,
-		float duty_max,
-		float lambda,
-		std::shared_ptr<arfModelLinearBase> arfModelPtr
-	):
-		m_gainP(gainP),
-		m_gainD(gainD),
-		m_gainI(gainI),
-		m_periodMux_us(periodMux_us),
-		m_periodControl_ms(periodControl_ms),
-		m_periodObs_ms(periodObs_ms),
-		m_interval_min(interval_min_us),
-		m_duty_max(duty_max),
-		m_lambda(lambda),
-		m_arfModelPtr(arfModelPtr),
-		m_isRunning(false),
-		m_isPaused(false),
-		m_logEnabled(false)
-	{}
-
-	std::shared_ptr<Manipulator> VarMultiplexManipulator::Create(
-		const Eigen::Vector3f& gainP,
-		const Eigen::Vector3f& gainD,
-		const Eigen::Vector3f& gainI,
-		int periodMux_us,
-		int periodControl_ms,
-		int periodObs_ms,
-		int interval_min_us,
-		float duty_max,
-		float lambda,
-		std::shared_ptr<arfModelLinearBase> arfModelPtr
-	) {
-		return std::make_shared<VarMultiplexManipulator>(
-			gainP,
-			gainD,
-			gainI,
-			periodMux_us,
-			periodControl_ms,
-			periodObs_ms,
-			interval_min_us,
-			duty_max,
-			lambda,
-			arfModelPtr
-			);
-	}
-
-	int VarMultiplexManipulator::StartManipulation(
-		std::shared_ptr<autd::Controller> pAupa,
-		std::shared_ptr<Tracker> pTracker,
-		FloatingObjectPtr pObject
-	) {
-		m_pAupa = pAupa;
-		m_pTracker = pTracker;
-		m_pObject = pObject;
-		if (!IsInitialized(pAupa, pTracker)) {
-			return 1;
-		}
-		timeBeginPeriod(1);
-		{
-			std::lock_guard<std::shared_mutex> lock(m_mtx_isRunning);
-			m_isRunning = true;
-		}
-		mux.SetStopSequence([this]() {
-			this->m_pAupa->AppendGainSync(autd::NullGain::Create());
-			}
-		);
-		m_thr_track = std::thread([this]()
-			{
-				while (this->IsRunning()) {
-					DWORD observationTime = timeGetTime();
-					navigator.Navigate(m_pTracker, m_pObject);
-					int waitTime = m_periodObs_ms - (observationTime - timeGetTime());
-					Sleep(std::max(waitTime, 0));
-				}
-			}
-		);
-		m_thr_control = std::thread([this]()
-			{
-				while (this->IsRunning()) {
-					if (IsPaused()) {
-						this->ExecuteOnPaused(m_pAupa, m_pObject);
-					}
-					else {
-						this->ExecuteSingleActuation(m_pAupa, m_pObject);
-					}
-				}
-			}
-		);
-		timeEndPeriod(1);
-		return 0;
-	}
-
-	void VarMultiplexManipulator::FinishManipulation() {
-		{
-			std::lock_guard<std::shared_mutex> lk(m_mtx_isRunning);
-			m_isRunning = false;
-		}
-		if (m_thr_track.joinable()) {
-			m_thr_track.join();
-		}
-		if (m_thr_control.joinable()) {
-			m_thr_control.join();
-		}
-		navigator.CloseLog();
-		mux.Stop();
-	}
-
-	void VarMultiplexManipulator::PauseManipulation() {
-		std::lock_guard<std::mutex> lk(m_mtx_isPaused);
-		m_isPaused = true;
-	}
-
-	void VarMultiplexManipulator::ResumeManipulation() {
-		std::lock_guard<std::mutex> lk(m_mtx_isPaused);
-		m_isPaused = false;
-	}
-
-	void VarMultiplexManipulator::ExecuteSingleActuation(
-		std::shared_ptr<autd::Controller> pAupa,
-		FloatingObjectPtr pObject
-	) {
-		DWORD timeLoopInit = timeGetTime();
-		Eigen::Vector3f pos, vel, integ;
-		pObject->getStates(pos, vel, integ);
-		if (pObject->IsTracked() && isInsideWorkspace(pObject->getPosition(), pObject->lowerbound(), pObject->upperbound())) {
-			auto posTgt = pObject->getPositionTarget(timeLoopInit);
-			auto velTgt = pObject->getVelocityTarget(timeLoopInit);
-			auto accelTgt = pObject->getAccelTarget(timeLoopInit);
-			Eigen::Vector3f accel;
-			{
-				std::lock_guard<std::mutex> lock(m_mtx_gain);
-				accel
-					= m_gainP.asDiagonal() * (pos - posTgt)
-					+ m_gainD.asDiagonal() * (vel - velTgt)
-					+ m_gainI.asDiagonal() * integ
-					+ accelTgt;
-			}
-			Eigen::Vector3f forceToApply
-				= pObject->totalMass() * accel
-				+ pObject->AdditionalMass() * Eigen::Vector3f(0, 0, GRAVITY_ACCEL);
-			auto duties = ComputeDuty(forceToApply, pos);
-			int num_active = (duties.array() > minimum_duty).count();
-			if (num_active == 0) {
-				mux.Stop();
-				m_pAupa->AppendGainSync(autd::NullGain::Create());
-			}
-			else {
-				//auto sequence = SplitSequence(
-				//	CreateDriveSequence(duties, pos),
-				//	m_interval_min
-				//	);
-				auto sequence = CreateDriveSequenceOld(duties, pos);
-				mux.Stop();
-				mux.ClearOrders();
-				for (int i = 0; i < sequence.size(); i++) {
-					mux.AddOrder(
-						[sequence, i, this]() { m_pAupa->AppendGainSync(sequence[i].first); },
-						sequence[i].second
-					);
-				}
-				mux.Start();
-			}
-			if (m_logEnabled) {
-				m_controlLogStream
-					<< timeLoopInit << ","
-					<< pos.x() << "," << pos.y() << "," << pos.z() << ","
-					<< vel.x() << "," << vel.y() << "," << vel.z() << ","
-					<< integ.x() << "," << integ.y() << "," << integ.z() << ","
-					<< posTgt.x() << "," << posTgt.y() << "," << posTgt.z() << ","
-					<< velTgt.x() << "," << velTgt.y() << "," << velTgt.z() << ","
-					<< accelTgt.x() << "," << accelTgt.y() << "," << accelTgt.z() << ","
-					<< accel.x() << "," << accel.y() << "," << accel.z() << ",";
-				Eigen::MatrixXf posRel = pos.replicate(1, m_pAupa->geometry()->numDevices()) - CentersAutd(m_pAupa->geometry());
-				Eigen::Vector3f forceResult = m_arfModelPtr->arf(posRel, RotsAutd(pAupa->geometry())) * duties;
-				m_controlLogStream << forceResult.x() << "," << forceResult.y() << "," << forceResult.z();
-				for (int i_duty = 0; i_duty < duties.size(); i_duty++) {
-					m_controlLogStream << "," << duties[i_duty];
-				}
-				m_controlLogStream << std::endl;
-			}
-		}
-		int waitTime = m_periodControl_ms - (timeGetTime() - timeLoopInit);
-		Sleep(std::max(0, waitTime));
-	}
-
-	Eigen::VectorXf VarMultiplexManipulator::ComputeDuty(const Eigen::Vector3f& force, const Eigen::Vector3f& position) {
-		Eigen::MatrixXf posRel = position.replicate(1, m_pAupa->geometry()->numDevices()) - CentersAutd(m_pAupa->geometry());
-		Eigen::MatrixXf directions = posRel.colwise().normalized();
-		Eigen::Array<bool, -1, -1> isActive = ((directions.transpose() * force.normalized()).array() > 0.17f);
-		if(isActive.count() == 0) {
-			return Eigen::VectorXf::Zero(m_pAupa->geometry()->numDevices());
-		}
-		Eigen::MatrixXf posRelActive(3, isActive.count());
-		std::vector<Eigen::Matrix3f> rotsAutd = RotsAutd(m_pAupa->geometry());
-		std::vector<Eigen::Matrix3f> rotsAutdActive(isActive.count());
-		int iAutdActive = 0;
-		for (int iAutd = 0; iAutd < m_pAupa->geometry()->numDevices(); iAutd++) {
-			if (isActive(iAutd)) {
-				posRelActive.col(iAutdActive) << posRel.col(iAutd);
-				rotsAutdActive[iAutdActive] = rotsAutd[iAutd];
-				iAutdActive++;
-			}
-		}
-		Eigen::MatrixXf F = m_arfModelPtr->arf(posRelActive, rotsAutdActive);
-		Eigen::VectorXi condEq(1);
-		condEq << -1;
-		Eigen::MatrixXf A(1, isActive.count());
-		A << Eigen::RowVectorXf::Ones(isActive.count());
-		Eigen::VectorXf b(1);
-		b << m_duty_max;
-		Eigen::VectorXf resultActive;
-		EigenCgalQpSolver(
-			resultActive,
-			A,
-			b,
-			F.transpose() * F + m_lambda * Eigen::MatrixXf::Identity(isActive.count(), isActive.count()),
-			-F.transpose() * force,
-			condEq,
-			Eigen::VectorXf::Zero(isActive.count()),
-			Eigen::VectorXf::Ones(isActive.count())
-		);
-		iAutdActive = 0;
-		Eigen::VectorXf resultFull(m_pAupa->geometry()->numDevices());
-		for (int iAutd = 0; iAutd < posRel.cols(); iAutd++) {
-			if (isActive(iAutd)) {
-				resultFull(iAutd) = resultActive(iAutdActive);
-				iAutdActive++;
-			}
-			else {
-				resultFull(iAutd) = 0;
-			}
-		}
-		return std::move(resultFull);
-	}
-
-	std::vector<std::pair<autd::GainPtr, int>> VarMultiplexManipulator::CreateDriveSequenceOld(
-		const Eigen::VectorXf& duties,
-		const Eigen::Vector3f& focus
-	) {
-		//std::vector<float> dutiesStl(duties.size());
-		//Eigen::Map<Eigen::VectorXf>(&dutiesStl[0], duties.size()) = duties;
-		int num_active = (duties.array() > minimum_duty).count();
-		int duration = m_periodMux_us / num_active;
-		//std::vector<autd::GainPtr> gain_list(num_active);
-		std::vector<std::pair<autd::GainPtr, int>> sequence;
-		for (int iAutd = 0; iAutd < m_pAupa->geometry()->numDevices(); iAutd++) {
-			if (duties(iAutd) > minimum_duty) {
-				std::map<int, autd::GainPtr> gain_map;
-				for (int i = 0; i < m_pAupa->geometry()->numDevices(); i++) {
-					if (i == iAutd) {
-						int amplitude = std::max(0, (std::min(255, static_cast<int>(duties(iAutd) * num_active * 255.f))));
-
-						gain_map.insert(std::make_pair(i, autd::FocalPointGain::Create(focus, amplitude)));
-					}
-					else {
-						gain_map.insert(std::make_pair(i, autd::NullGain::Create()));
-					}
-				}
-				auto gain = autd::GroupedGain::Create(gain_map);
-				sequence.push_back(std::make_pair(gain, duration));
-			}
-		}
-		//int id_begin_search = 0;
-		//for (auto itr_list = sequence.begin(); itr_list != sequence.end(); itr_list++) {
-		//	std::map<int, autd::GainPtr> gain_map;
-		//	auto itr_duties = std::find_if(dutiesStl.begin() + id_begin_search, dutiesStl.end(), [](float u) {return u > minimum_duty; });
-		//	for (int i_autd = 0; i_autd < m_pAupa->geometry()->numDevices(); i_autd++) {
-		//		if (i_autd == std::distance(dutiesStl.begin(), itr_duties)) {
-		//			int amplitude = std::max(0, (std::min(255, static_cast<int>((*itr_duties) * num_active * 255.f))));
-		//			gain_map.insert(std::make_pair(i_autd, autd::FocalPointGain::Create(focus, amplitude)));
-		//		}
-		//		else {
-		//			gain_map.insert(std::make_pair(i_autd, autd::NullGain::Create()));
-		//		}
-		//	}
-		//	auto gain = autd::GroupedGain::Create(gain_map);
-		//	*itr_list = std::make_pair(gain, duration);
-		//	id_begin_search = std::distance(dutiesStl.begin(), itr_duties) + 1;
-		//}
-		return sequence;
-	}
-
-	std::vector<std::pair<autd::GainPtr, int>> VarMultiplexManipulator::CreateDriveSequence(
-		const Eigen::VectorXf& duty, 
-		const Eigen::Vector3f& focus
-	) {
-		auto amplitude = static_cast<int>(255 * std::max(0.0f, std::min(1.0f, duty.sum())));
-		std::vector<std::pair<autd::GainPtr, int>> sequence;
-		for (int iAutd = 0; iAutd < m_pAupa->geometry()->numDevices(); iAutd++) {
-			if (duty(iAutd) > minimum_duty) {
-				std::map<int, autd::GainPtr> gain_map;
-				for (int i = 0; i < m_pAupa->geometry()->numDevices(); i++) {
-					if (i == iAutd) {
-						gain_map.insert(std::make_pair(i, autd::FocalPointGain::Create(focus, amplitude)));
-					}
-					else {
-						gain_map.insert(std::make_pair(i, autd::NullGain::Create()));
-					}
-				}
-				auto gain = autd::GroupedGain::Create(gain_map);
-				auto duration = static_cast<int>(m_periodMux_us * duty(iAutd) / duty.sum());
-				sequence.push_back(std::make_pair(gain, duration));
-			}
-		}
-		return sequence;
-	}
-
-	std::vector<std::pair<autd::GainPtr, int>>
-		VarMultiplexManipulator::SplitSequence(const std::vector<std::pair<autd::GainPtr, int>>& sequence, int interval) {
-		std::vector<std::pair<autd::GainPtr, int>> sequence_new;
-		for (auto itr = sequence.cbegin(); itr != sequence.cend(); itr++) {
-			int num_split = itr->second / interval;
-			for (int i_split = 0; i_split < num_split; i_split++) {
-				sequence_new.push_back(std::make_pair(itr->first, interval));
-			}
-			sequence_new.push_back(std::make_pair(itr->first, itr->second - num_split * interval));
-		}
-		return sequence_new;
-	}
-
-
-	void VarMultiplexManipulator::ExecuteOnPaused(
-		std::shared_ptr<autd::Controller> pAupa,
-		FloatingObjectPtr pObject
-	) {
-		pAupa->AppendGainSync(autd::FocalPointGain::Create(pObject->getPosition()));
-		pAupa->AppendModulationSync(autd::SineModulation::Create(150));
-		std::this_thread::sleep_for(std::chrono::milliseconds(30));
-		return;
-	}
-
-	void VarMultiplexManipulator::SetGain(
-		const Eigen::Vector3f& gainP,
-		const Eigen::Vector3f& gainD,
-		const Eigen::Vector3f& gainI
-	) {
-		std::lock_guard<std::mutex> lock(m_mtx_gain);
-		m_gainP = gainP;
-		m_gainD = gainD;
-		m_gainI = gainI;
-	}
-
-	std::shared_ptr<arfModelLinearBase> VarMultiplexManipulator::arfModel() {
-		return m_arfModelPtr;
-	}
-
-	bool VarMultiplexManipulator::IsRunning() {
-		std::shared_lock<std::shared_mutex> lock(m_mtx_isRunning);
-		return m_isRunning;
-	}
-
-	bool VarMultiplexManipulator::IsPaused() {
-		std::lock_guard<std::mutex> lock(m_mtx_isPaused);
-		return m_isPaused;
-	}
-
-	void VarMultiplexManipulator::EnableLog(
-		const std::string& obsLogName,
-		const std::string& controlLogName
-	) {
-		if (!IsRunning()) {
-			this->navigator.InitLog(obsLogName);
-			this->m_controlLogName = controlLogName;
-			this->m_logEnabled = true;
-		}
-	}
-
-	void VarMultiplexManipulator::DisableLog() {
-		this->navigator.CloseLog();
-		this->m_logEnabled = false;
 	}
 }

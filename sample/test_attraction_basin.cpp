32b801b417d27c13cabc09bbf4281b34f3b1ae2f
Takuro Furumoto <furumoto@hapis.k.u-tokyo.ac.jp>
2022-02-18 14:23:16 +0900

    test_attraction_basin: add time-mux driving model and obsevation-model

--- sample/test_attraction_basin.cpp
+++ sample/test_attraction_basin.cpp
@@ -15,6 +15,10 @@ using state_type = std::vector<float>;
 constexpr auto GRAVITY_ACCEL = 9.80665e3f; 
 constexpr auto RHO = 1.168e-9; //[kg/mm3]
 constexpr auto DUTY_MIN = 1.0f / 255.0f;
+constexpr float DT_MUX = 0.0025f;
+constexpr int NUM_STEP_MAX = 1000;
+constexpr float DT_STEP = 0.0005f;
+constexpr float DT_OBS = 0.01f;
 
 float MuxMaximumThrust(
 	const Eigen::Vector3f& posStart,
@@ -95,7 +99,7 @@ public:
 	:pTrajectory(pTrajectory)
 	{
 		ofs.open(filename);
-		ofs << "t,x,y,z,vx,vy,vz,ix,iy,iz,xTgt,yTgt,zTgt" << std::endl;
+		ofs << "t,x,y,z,vx,vy,vz,xTgt,yTgt,zTgt" << std::endl;
 	}
 	~Logger() {
 		ofs.close();
@@ -118,49 +122,118 @@ private:
 	FloatingObjectPtr pObject_;
 	Eigen::MatrixXf centersAupa_;
 	std::vector<Eigen::Matrix3f> rotsAupa_;
+	std::ofstream ofsObs_;
+	int step_count_ = 0;
+public:
+	std::vector<float> times_;
+	std::vector<std::pair<size_t, float>> duties_;
 public:
 	System(
 		std::shared_ptr<autd::Controller> pAupa,
 		std::shared_ptr<dynaman::Tracker> pTracker,
-		FloatingObjectPtr pObject
+		FloatingObjectPtr pObject,
+		const std::string& filename
 	):
 		manipulator_(pAupa, pTracker),
 		pObject_(pObject),
 		centersAupa_(CentersAutd(pAupa->geometry())),
 		rotsAupa_(RotsAutd(pAupa->geometry()))
 	{
+		ofsObs_.open(filename);
+	}
+
+	void addDriveSequence(
+		const float t,
+		const Eigen::VectorXf& duty,
+		std::vector<float>& times,
+		std::vector<std::pair<size_t, float>> &duties
+	) {
+		int num_active = (duty.array() > DUTY_MIN).count();
+		if (num_active == 0) {
+			times.push_back(t);
+			duties.push_back(std::make_pair(0, 0));
+		}
+		else {
+			int deviceCount = 0;
+			for (int iDevice = 0; iDevice < 11; iDevice++) {
+				if (duty[iDevice] > DUTY_MIN) {
+					times.push_back(t + DT_OBS / num_active * deviceCount);
+					duties.push_back(std::make_pair(iDevice, duty[iDevice]));
+					deviceCount++;
+				}
+			}
+		}
+	}
+
+	void observe(const state_type& x, const float t) {
+		if (step_count_ % static_cast<int>(std::roundf(DT_OBS / DT_STEP)) == 0) {
+			//observation
+			DWORD systime_ms = static_cast<DWORD>(1000 * t);
+			Eigen::Vector3f pos(x[0], x[1], x[2]);
+			pObject_->updateStates(systime_ms, pos);
+
+			//actuation
+			Eigen::Vector3f vel, integ;
+
+			pObject_->getStates(pos, vel, integ);
+			auto posTgt = pObject_->getPositionTarget(systime_ms);
+			auto velTgt = pObject_->getVelocityTarget(systime_ms);
+			auto accelTgt = pObject_->getAccelTarget(systime_ms);
+			Eigen::Vector3f accelTarget
+				= manipulator_.gainP().asDiagonal() * (posTgt - pos)
+				+ manipulator_.gainD().asDiagonal() * (velTgt - vel)
+				- manipulator_.gainI().asDiagonal() * integ
+				+ accelTgt;
+
+			Eigen::Vector3f forceTarget
+				= pObject_->totalMass() * accelTarget
+				+ pObject_->AdditionalMass() * Eigen::Vector3f(0.f, 0.f, GRAVITY_ACCEL);
+			Eigen::MatrixXf posRel = pos.replicate(1, 11) - centersAupa_;
+
+			Eigen::VectorXf duty = manipulator_.ComputeDuty(forceTarget, pos);
+			int num_active = (duty.array() > DUTY_MIN).count();
+			duty = (duty * num_active).cwiseMin(1.0f).cwiseMax(0.0f);
+			addDriveSequence(
+				t,
+				duty,
+				times_,
+				duties_
+			);
+			ofsObs_
+				<< t
+				<< "," << pos.x() << "," << pos.y() << "," << pos.z()
+				<< "," << vel.x() << "," << vel.y() << "," << vel.z()
+				<< "," << integ.x() << "," << integ.y() << "," << integ.z();
+			for (int i = 0; i < duty.size(); i++) {
+				ofsObs_ << "," << duty[i];
+			}
+			ofsObs_ << std::endl;
+		}
+		step_count_++;
 	}
 
 	void operator()(const state_type& x, state_type& dxdt, const float t) {
-		//previous state is saved in FloatingObject
 		//observation
 		DWORD systime_ms = static_cast<DWORD>(t * 1000);
-		Eigen::Vector3f pos_true(x[0], x[1], x[2]);
-		Eigen::Vector3f vel_true(x[3], x[4], x[5]);
-		Eigen::Vector3f integ_true(x[6], x[7], x[8]);
+		Eigen::Vector3f pos(x[0], x[1], x[2]);
+		Eigen::Vector3f vel(x[3], x[4], x[5]);
 
-		//actuation
-		auto posTgt = pObject_->getPositionTarget(systime_ms);
-		auto velTgt = pObject_->getVelocityTarget(systime_ms);
-		auto accelTgt = pObject_->getAccelTarget(systime_ms);
-		Eigen::Vector3f accelTarget
-			= manipulator_.gainP().asDiagonal() * (posTgt - pos_true)
-			+ manipulator_.gainD().asDiagonal() * (velTgt - vel_true)
-			- manipulator_.gainI().asDiagonal() * integ_true
-			+ accelTgt;
+		Eigen::MatrixXf posRel = pos.replicate(1, 11) - centersAupa_;
 
-		Eigen::Vector3f forceTarget
-			= pObject_->totalMass() * accelTarget
-			+ pObject_->AdditionalMass() * Eigen::Vector3f(0.f, 0.f, GRAVITY_ACCEL);
-		Eigen::MatrixXf posRelTrue = pos_true.replicate(1, 11) - centersAupa_;
+		size_t iDevice = 0;
+		float duty = 0;
+		for (auto itr = times_.rbegin(); itr != times_.rend(); itr++) {
+			if (*itr <= t) {
+				auto it = std::distance(itr, times_.rend())-1;
+				iDevice = duties_[it].first;
+				duty = duties_[it].second;
+				break;
+			}
+		}
 
-		Eigen::VectorXf duty = manipulator_.ComputeDuty(forceTarget, pos_true);
-		int num_active = (duty.array() > DUTY_MIN).count();
-		duty = (duty * num_active).cwiseMin(1.0f).cwiseMax(0.0f);
-		Eigen::MatrixXf FTrue = manipulator_.arfModel()->arf(posRelTrue, rotsAupa_);
-
-		Eigen::Vector3f forceArf = FTrue * duty;
-		Eigen::Vector3f drag = -0.5f * pi * RHO * vel_true.norm() * vel_true.norm() * pObject_->Radius() * pObject_->Radius() * vel_true.normalized();
+		Eigen::MatrixXf FTrue = manipulator_.arfModel()->arf(posRel, rotsAupa_);
+		Eigen::Vector3f forceArf = FTrue.col(iDevice)* duty;
+		Eigen::Vector3f drag = -0.5f * pi * RHO * vel.norm() * vel.norm() * pObject_->Radius() * pObject_->Radius() * vel.normalized();
 		Eigen::Vector3f forceTotal = forceArf + drag;
 		Eigen::Vector3f accelResult = forceTotal / pObject_->totalMass();
 		dxdt[0] = x[3];
@@ -169,9 +242,6 @@ public:
 		dxdt[3] = accelResult[0];
 		dxdt[4] = accelResult[1];
 		dxdt[5] = accelResult[2];
-		dxdt[6] = x[0] - posTgt.x();
-		dxdt[7] = x[1] - posTgt.y();
-		dxdt[8] = x[2] - posTgt.z();
 	}
 };
 
@@ -181,28 +251,29 @@ int main(int argc, char** argv) {
 
 	Eigen::Vector3f posInitTgt(-200, 0, 0);
 	Eigen::Vector3f posEndTgt(200, 0, 0);
-	Eigen::Vector3f posError(0, 0, 0);
+	Eigen::Vector3f posError(0, 0, 100);
 	Eigen::Vector3f velError(0, 0, 0);
-	state_type state(9);
+	state_type state(6);
 	state[0] = posInitTgt.x() + posError.x();
 	state[1] = posInitTgt.y() + posError.y();
 	state[2] = posInitTgt.z() + posError.z();
 	state[3] = velError[0];
 	state[4] = velError[1];
 	state[5] = velError[2];
-	state[6] = 0;
-	state[7] = 0;
-	state[8] = 0;
 
 	float time_init = 1.0f;
 	float time_trans_start = 2.0f;
-	float time_end = 5.0f;
+	float time_end = 4.0f;
 	float duty_ff_max = 0.5f;
 
-	constexpr int NUM_STEP_MAX = 1000;
-	constexpr float DT_STEP = 0.001f;
 
 	// ***********************************
+	char date[sizeof("yymmdd_HHMMSS")];
+	auto tt = std::time(nullptr);
+	std::strftime(&date[0], sizeof(date), "%y%m%d_%H%M%S", std::localtime(&tt));
+	std::string prefix(date);
+	std::string filenameTrue = prefix + "_basin_true_log.csv";
+	std::string filenameObs = prefix + "_basin_obs_log.csv";
 
 	auto pAupa = haptic_icon::CreateController();
 	auto pTracker = haptic_icon::CreateTracker("blue_target_r50mm.png");
@@ -212,7 +283,7 @@ int main(int argc, char** argv) {
 		Eigen::Vector3f::Constant(500),
 		50.0f
 	);
-	System system(pAupa, pTracker, pObject);
+	System system(pAupa, pTracker, pObject, filenameObs);
 	
 	auto pTrajectory = CreateBangbangTrajecotryWithDrag(
 		pAupa,
@@ -225,13 +296,7 @@ int main(int argc, char** argv) {
 	
 	pObject->SetTrajectory(pTrajectory);
 
-	char date[sizeof("yymmdd_HHMMSS")];
-	auto tt = std::time(nullptr);
-	std::strftime(&date[0], sizeof(date), "%y%m%d_%H%M%S", std::localtime(&tt));
-	std::string filename(date);
-	filename += "basin_log.csv";
-	Logger logger(filename, pTrajectory);
-
+	Logger logger(filenameTrue, pTrajectory);
 
 	runge_kutta4<state_type> stepper;
 	auto steps = integrate_const(
@@ -241,7 +306,10 @@ int main(int argc, char** argv) {
 		time_init,
 		time_end,
 		DT_STEP,
-		std::ref(logger)
+		[&system, &logger](const state_type& x, const float t) {
+			logger(x, t);
+			system.observe(x, t);
+		}
 	);
 	
 	std::cout << "Propagation finished. Total number of steps: " << steps << std::endl;
